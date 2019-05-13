299007fdd8aa92b41ed851897db9366280d4645b
Jaffar3600 <45350559+Jaffar3600@users.noreply.github.com>
2019-05-13 09:19:49 +0000

    no changes

---
+++ Banking/Controllers/AccountDetailsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Banking.Models;
+
+namespace Banking.Controllers
+{   
+    [Authorize]
+    public class AccountDetailsController : Controller
+    {
+        private BankingEntities db = new BankingEntities();
+
+        // GET: AccountDetails
+        public ActionResult Index()
+        {
+            return View(db.AccountDetails.ToList());
+        }
+
+        // GET: AccountDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountDetail accountDetail = db.AccountDetails.Find(id);
+            if (accountDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountDetail);
+        }
+
+        // GET: AccountDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AccountDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AccountId,AccountHolderName,AccountBalance,AccountType,MobileNumber,State,City")] AccountDetail accountDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AccountDetails.Add(accountDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(accountDetail);
+        }
+        [Authorize(Roles ="Admin")]
+        // GET: AccountDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountDetail accountDetail = db.AccountDetails.Find(id);
+            if (accountDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountDetail);
+        }
+
+        // POST: AccountDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AccountId,AccountHolderName,AccountBalance,AccountType,MobileNumber,State,City")] AccountDetail accountDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(accountDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(accountDetail);
+        }
+        [Authorize(Roles ="Admin")]
+
+        // GET: AccountDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountDetail accountDetail = db.AccountDetails.Find(id);
+            if (accountDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountDetail);
+        }
+
+        // POST: AccountDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AccountDetail accountDetail = db.AccountDetails.Find(id);
+            db.AccountDetails.Remove(accountDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Banking/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Banking.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public int UserName { get; set; }
+        public int Password { get; set; }
+        public int Role { get; set; }
+    }
+}
